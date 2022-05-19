76849e8fa71a90e4f8af3aa0bce3ad5c0e8c926d
magnusakselvoll <magnus.akselvoll@mercell.com>
2022-05-19 09:19:55 +0200

    Package versioning

--- write-version-variable.ps1
+++ write-version-variable.ps1
@@ -1,4 +1,4 @@
 $version = Get-Content .\version.json -Raw | ConvertFrom-Json | Select-Object -ExpandProperty PackageVersion
 Write-Host ("Nuget package version: $version")
-Write-Host ("##vso[task.setvariable variable=PackageVersion;]$version")
+Write-Output "NUGET_PACKAGE_VERSION=$version" | Out-File -FilePath $Env:GITHUB_ENV -Encoding utf-8 -Append
 
